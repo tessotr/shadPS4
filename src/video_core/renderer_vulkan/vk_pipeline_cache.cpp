80bd4cd6b1a97c9efb3b7f1ce4fe5aae07c1675a
Texyc <97372940+tessotr@users.noreply.github.com>
2024-09-04 20:52:40 -0400

    more workflow changes + shader skips

--- src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
+++ src/video_core/renderer_vulkan/vk_pipeline_cache.cpp
@@ -179,8 +179,10 @@ const ComputePipeline* PipelineCache::GetComputePipeline() {
 }
 
 bool ShouldSkipShader(u64 shader_hash, const char* shader_type) {
-    static constexpr std::array<u64, 7> skip_hashes = {
-        0x42f2a521, 0x2da7fe60, 0x8e3f8dc4, 0xa509af23, 0x4ca76892, 0xa954e79d, 0x1635154c};
+    static constexpr std::array<u64, 22> skip_hashes = {
+        0xa509af23, 0x4ca76892, 0xa954e79d, 0x42f2a521, 0x2da7fe60, 0x1635154c, 0x8e3f8dc4,
+        0xc0cbc309, 0x77d1c63,  0xff7a6d7c, 0xddfbac23, 0x4899010a, 0xc8854a11, 0xaa9d023d,
+        0x17a64a21, 0x94ec4dfb, 0xbddb8fc7, 0x733dae6f, 0x9a987165, 0x70ffb249, 0xbfed1ef4, 0xb3a9807b};
     if (std::ranges::contains(skip_hashes, shader_hash)) {
         LOG_WARNING(Render_Vulkan, "Skipped {} shader hash {:#x}.", shader_type, shader_hash);
         return true;
